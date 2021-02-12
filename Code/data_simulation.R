f6bb99a43ddb0217896cf84cb4fb07f65d69aaa2
Samantha Bothwell <samantha.bothwell@cuanschutz.edu>
2021-02-12 10:55:08 -0700

    update simulation code

---
+++ Code/data_simulation.R
@@ -0,0 +1,70 @@
+#########################################################
+#########################################################
+####    BIOS 7721 Final Project : Simulation Study   ####
+####                                                 ####
+#### Author : Samantha Bothwell                      ####
+####                                                 ####
+#### Professor : Krithika Suresh                     ####
+####                                                 ####
+#### File Purpose : Simulate longitudinal marker     ####
+####    values from a given distribution and create  ####
+####    random visit times from a Poisson distibution####
+####                                                 ####
+#### Last Update : 2/12/2021                         ####
+#########################################################
+#########################################################
+
+rm(list=ls())
+
+## Packages 
+
+
+# Simulation settings 
+N <- 250 #number of simulated individuals 
+K <-  # number of planned repeated measurements per subject, per outcome
+cens_horiz <-  #maximum follow-up time
+insp.rate <-  #rate at which inspection times are made
+  
+  # True parameter values ---------------------------------------------------
+betas <- c(-0.2, 0.3) #betas for longitudinal model
+sigma.y <-  0.5 #measurement error standard deviation
+A <- matrix(c(0.5, 0, 0, 0.3), ncol = 2) #covariance matrix for random effects
+alpha <- 0.6 #association parameter
+h0 <- 0.1 #constant baseline hazard
+                                                 0    A_22
+
+  
+# Longitudinal submodel ---------------------------------------------------
+# 1. Generate the id variable (each individual has K measurements)
+id <- 
+  
+# 2. Generate the times at which inidividuals have their longitudinal measurements
+# Fixed measurement times: 
+# times <- c(replicate(N, 0:(K-1)))
+# Random measurement times: 
+# times <- c(replicate(N, c(0, sort(runif(K-1, 0, t.max))))) #uniform 
+# Can you do it for a poisson process? (Hint: you might need the "cumsum" function)
+msmt.times <- 
+
+# 3. Begin creating your data set
+dat <- 
+
+# 4. Create design matrices X and Z 
+# X: design matrix for fixed effects (should have as many columns as elements in "betas")
+# Z: design  matrix for random effects (should have as many columns as elements in "A")
+X <- 
+Z <- 
+
+# 5. Simulate random effects
+# b: simulated random effects b using covariance matrix A 
+b <- 
+
+# 6. Simulate longitudinal responses
+eta.y <- #Xi'(t)*beta+zi'(t)*bi
+y <- #~Normal(mean=eta.y, sd=sigma.y) 
+
+# 7. Add longitudinal responses to data set 
+                                                   
+                                                   
+                                                   
+                                                   
