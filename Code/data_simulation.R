8656a98229b2c9c339df288d36d193f8a14237fb
Samantha Bothwell <samantha.bothwell@cuanschutz.edu>
2021-02-25 16:14:07 -0700

    finish survival simulation

--- Code/data_simulation.R
+++ Code/data_simulation.R
@@ -10,19 +10,19 @@
 ####    values from a given distribution and create  ####
 ####    random visit times from a Poisson distibution####
 ####                                                 ####
-#### Last Update : 2/12/2021                         ####
+#### Last Update : 2/24/2021                         ####
 #########################################################
 #########################################################
 
 rm(list=ls())
 
-## Packages 
-
+## Packages
+library(survival)
 
 # Simulation settings
 set.seed(7721)
 N <- 250 #number of simulated individuals 
-K <- sample(1:10, N, replace = T) # number of planned repeated measurements per subject, per outcome
+K <- 10 # sample(1:10, N, replace = T) # number of planned repeated measurements per subject, per outcome
 cens_horiz <- 10 #maximum follow-up time
 insp.rate <- 1 #rate at which inspection times are made
 
@@ -37,7 +37,7 @@ h0 <- 0.1 #constant baseline hazard
   
 # Longitudinal submodel ---------------------------------------------------
 # 1. Generate the id variable (each individual has K measurements)
-id <- rep(1:N, K)
+id <- rep(1:N, each = K)
   
 # 2. Generate the times at which inidividuals have their longitudinal measurements
 # Fixed measurement times: 
@@ -45,14 +45,10 @@ id <- rep(1:N, K)
 # Random measurement times: 
 # times <- c(replicate(N, c(0, sort(runif(K-1, 0, t.max))))) #uniform 
 # Can you do it for a poisson process? (Hint: you might need the "cumsum" function)
-msmt.times <- vector()
-for (i in 1:length(K)){
-  msmt.times <- c(msmt.times, c(0, cumsum(rpois(K[i] - 1, 1))))
-}
-
+msmt.times <- c(replicate(N, c(0, cumsum(rexp(K - 1, insp.rate)))))
 
 # 3. Begin creating your data set
-dat <- data.frame(ID = id, Year = msmt.times)
+dat <- data.frame(ID = id, Time = msmt.times)
 
 # 4. Create design matrices X and Z 
 # X: design matrix for fixed effects (should have as many columns as elements in "betas")
@@ -62,14 +58,19 @@ Z <- matrix(cbind(rep(1, length(id)), msmt.times), ncol = 2)
 
 # 5. Simulate random effects
 # b: simulated random effects b using covariance matrix A 
-b <- c(mean(rnorm(1, 0, A[1])), mean(rnorm(1, 0, A[4])))
+b <- cbind(rnorm(N, 0, A[1]), rnorm(N, 0, A[4]))
 
 # 6. Simulate longitudinal responses
-eta.y <- t(X)*betas + t(Z)*b #Xi'(t)*beta+zi'(t)*bi
-y <- rnorm(length(id), mean=eta.y, sd = sigma.y) #~Normal(mean=eta.y, sd=sigma.y) 
+index = 1
+eta.y = vector()
+for (i in 1:N){
+  eta.y[c(index:(index+9))] <- X[c(index:(index+9)),] %*% betas + Z[c(index:(index+9)),] %*% b[i,] #Xi'(t)*beta+zi'(t)*bi
+  index = index + 10
+}
+y <- rnorm(K*N, mean=eta.y, sd = sigma.y) #~Normal(mean=eta.y, sd=sigma.y) 
 
 # 7. Add longitudinal responses to data set 
-dat$resp <- y 
+dat$y <- y 
 
 
 
@@ -84,13 +85,13 @@ invS <- function (t, u, i) {
   #i: patient id indicator 
   h <- function (s) {
     #hazard function (based on survival and longitudinal submodels)
-    XX <- 
-      ZZ <-  
-      m <-  
-      haz <- h0*exp(alpha*m)
+    XX <- cbind(1, s) %*% betas
+    ZZ <- cbind(1, s) %*% b[i,]   
+    m <- XX + ZZ
+    haz <- h0*exp(alpha*m)
     return(haz)
   }
-  integrate(h, lower = 0, upper = t)$value + log(u) 
+  integrate(h, lower = 0, upper = t)$value + log(u) # u is (1-u) but that is also uniform
 }
 
 # 2. Solve for survival times 
@@ -99,36 +100,40 @@ trueTimes <- numeric(N) #initiate vector of true survival times
 for (i in 1:N) {
   # Solve for true survival time (Hint: use "uniroot" function)
   # Wrap the uniroot function in "try" since for some individuals you will not return a valid 
-  # survival time (we will consider those patients "cured" and assign then survival time of
+  # survival time (we will consider those patients "cured" and assign them survival time of
   # infinity)
-  Root <- #Example: try(uniroot(, TRUE))
-    # "Cure" patients have event time set to Infinity 
-    trueTimes[i] <- ifelse(inherits(Root, "try-error"),Inf,Root)
+  surv <- function(t){ invS(t, u = u[i], i = i)}
+  Root <- try(uniroot(surv, interval = c(0, 10))$root)
+  # "Cure" patients have event time set to Infinity 
+  trueTimes[i] <- ifelse(inherits(Root, "try-error"),Inf,Root)
 }
 
 # 3. Simulate censoring times 
-Ctimes <- 
+Ctimes <- runif(N, min = 0, max = 10)
   
-  # 4. Compute observed survival time and event indicator (don't forget that there 
-  # is a max follow-up time)
-  Time <-  
-  event <-  
+# 4. Compute observed survival time and event indicator (don't forget that there 
+# is a max follow-up time)
+Time <- ifelse(Ctimes < trueTimes, Ctimes, trueTimes)
+event <- ifelse(Ctimes < trueTimes, 0,  1)
+
   
-  
-  # 5. Add in survival time and indicator to data set 
-  
-  
-  # 6. Drop measurement times that occur after follow-up time 
-  # This should then be your longitudinal data set
-  
-  # 7. Create a data set with one row for each individual (for survival submodel)
-  
-  # Plots of simulated data set -----------------------------------------------------------------
+# 5. Add in survival time and indicator to data set 
+dat$Survtime = rep(Time, each = K)
+dat$event = rep(event, each = K)
+
+# 6. Drop measurement times that occur after follow-up time 
+# This should then be your longitudinal data set
+dat <- dat[dat$Time <= dat$Survtime,]
+
+# 7. Create a data set with one row for each individual (for survival submodel)
+dat <- dat %>% group_by(ID) %>% slice(1)
+
+# Plots of simulated data set -----------------------------------------------------------------
 # Does your simulated data look correct? 
 ## Consider: KM curves 
-# plot(Surv(survtime,event)~1, data=dat)
+# plot(Surv(Survtime,event)~1, data=dat)
 # ## Consider: plot of the longitudinal biomarker 
-# table(table(dat$id))
+# table(table(dat$ID))
 
 
 # Fit models --------------------------------------------------------------
@@ -151,7 +156,6 @@ Ctimes <-
 
 
 
-
-                                                   
+  
                                                    
                                                    
